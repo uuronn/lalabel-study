c71b9c5bdf1e03dcaf3438b826b3af8144f1e686
uuronn <uuronprogram@gmail.com>
2024-05-26 00:16:59 +0900

    ユーザー登録機能の実装、 登録が成功するとcomplieteページへ遷移する

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,9 @@ Route::get('/home', function () {
     return view('home');
 });
 
+Route::get("/register", [\App\Http\Controllers\RegisterController::class, 'create'])
+    ->middleware('guest')
+    ->name('register');
+
+Route::post("/register", [\App\Http\Controllers\RegisterController::class, 'store'])
+    ->middleware('guest');
